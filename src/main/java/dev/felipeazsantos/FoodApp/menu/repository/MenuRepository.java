71f623c8b4d82544cb7c56df8ef3ed4473a764a8
Felipe Azevedo <felipeasantos10@hotmail.com>
2026-07-14 21:57:37 -0300

    creating menu entity and dtos

---
+++ src/main/java/dev/felipeazsantos/FoodApp/menu/repository/MenuRepository.java
@@ -0,0 +1,8 @@
+package dev.felipeazsantos.FoodApp.menu.repository;
+
+import dev.felipeazsantos.FoodApp.menu.entity.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface MenuRepository extends JpaRepository<Menu, Long>, JpaSpecificationExecutor<Menu> {
+}
