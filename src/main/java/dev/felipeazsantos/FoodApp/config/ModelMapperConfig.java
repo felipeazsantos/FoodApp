47844e7bd5e74a4bd712480f43c794f61d1f174c
Felipe Azevedo <felipeasantos10@hotmail.com>
2026-07-21 18:22:30 -0300

    model mapper config

---
+++ src/main/java/dev/felipeazsantos/FoodApp/config/ModelMapperConfig.java
@@ -0,0 +1,21 @@
+package dev.felipeazsantos.FoodApp.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        var modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setMatchingStrategy(MatchingStrategies.STANDARD);
+
+        return modelMapper;
+    }
+}
