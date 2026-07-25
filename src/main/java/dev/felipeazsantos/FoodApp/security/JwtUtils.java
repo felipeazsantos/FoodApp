f326a14411bf50834132e01d3901656912057a44
Felipe Azevedo <felipeasantos10@hotmail.com>
2026-07-25 13:04:19 -0300

    auth filter authentication app

--- src/main/java/dev/felipeazsantos/FoodApp/security/JwtUtils.java
+++ src/main/java/dev/felipeazsantos/FoodApp/security/JwtUtils.java
@@ -2,7 +2,6 @@ package dev.felipeazsantos.FoodApp.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
